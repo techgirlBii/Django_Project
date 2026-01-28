a454e6e6b03e9510333db471f2a7655548d71c49
Rasheedah <rasheedaholadunjoye@gmail.com>
2026-01-28 13:24:42 +0100

    Displaying Data on Templates

--- my_first_project/my_nova_app/urls.py
+++ my_first_project/my_nova_app/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('my_nova_app/', views.my_nova_app, name='intro'),
+    path('my_nova_app/more/<int:id>/', views.details, name='details'),
 ]
--- my_first_project/my_nova_app/views.py
+++ my_first_project/my_nova_app/views.py
@@ -1,11 +1,20 @@
 from django.http import HttpResponse
 from django.template import loader
+from .models import Person
 
 def my_nova_app(request):
-  persos = Item.objects.all().values()
-  template = loader.get_template('all_items.html')
+  persons = Person.objects.all().values()
+  template = loader.get_template('persons.html')
   context = {
-    'myitems': myitems,
+    'persons': persons,
+  } 
+  return HttpResponse(template.render(context, request))
+
+def details(request, id):
+  persons = Person.objects.get(id=id)
+  template = loader.get_template('more.html')
+  context = {
+    'persons': persons,
   }
   return HttpResponse(template.render(context, request))
 
